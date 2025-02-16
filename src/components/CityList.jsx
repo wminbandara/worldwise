fade612cf693f24b642c6c01bd55701b3ef4d7df
wminbandara <wminbandara@gmail.com>
2025-02-16 21:09:03 +0530

    12. Implementing the Countries List

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,16 @@
-import Spinner from "./spinner";
+import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
-import { mergeConfig } from "vite";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   return (
     <ul className={styles.CityList}>
       {cities.map((city) => (
